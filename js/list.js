9b4464e23056cc3e9d133a807d8888f37ba39d71
auroramc19 <auroramendezc.19@gmail.com>
2021-06-12 14:33:00 -0500

    avance lista

--- js/list.js
+++ js/list.js
@@ -92,7 +92,7 @@ var eliminar_ini =
         <p> }</p> 
    </div>
    `;
-var eliminar_lpp =
+var eliminar_lpp_end =
     `<div>
         <p> int sup_en_lista (Lista *lista, int pos){</p> 
         <p> &nbsp;&nbsp;&nbsp;&nbsp;if (lista->tamaño <= 1 || pos < 1 || pos >= lista->tamaño)</p>
@@ -126,47 +126,310 @@ canvas.height = height;
 canvas.style.background = "#ff8";
 
 //constants
-var ancho = 70;
-var alto = 60;
+//var ancho = 70;
+//var alto = 60;
 var attempt = 0;
+
+var xposx = 0;
+var yposy = 0;
 
 const yC = height / 2;
 const xCoordinate = 3 / 2;
 
+var arrowArray = new Array();
 var rectArray = new Array();
 class Rectangle {
     constructor(xpos, ypos, number) {
         this.xpos = xpos;
         this.ypos = ypos;
         this.number = number;
+        this.ancho = 0;
+        this.alto = 0;
+        this.show = 0;
         //this.dx = 8 - ((attempt) * (xCoordinate));
     }
-    draw(context) {
+    draw() {
         context.beginPath();
         context.textAlign = "center";
         context.textBaseline = "middle";
         //context.strokeStyle = "green";
         context.font = "20px Arial";
-        context.fillText(this.number, this.xpos + (ancho / 2), this.ypos + (alto / 2));
-        context.rect(this.xpos, this.ypos, ancho, alto);
-        context.rect(this.xpos + ancho, this.ypos, ancho, alto);
+        if (this.show == 1) context.fillText(this.number, this.xpos + (this.ancho / 2), this.ypos + (this.alto / 2));
+        context.lineWidth = 1;
+        context.rect(this.xpos, this.ypos, this.ancho, this.alto);
+        context.rect(this.xpos + this.ancho, this.ypos, this.ancho, this.alto);
         context.stroke();
     }
 }
 
-function draw(code) {
-    let myRect = new Rectangle(60, 100, 'a');
-    myRect.draw(context);
-    myRect = new Rectangle(260, 100, 'a');
-    myRect.draw(context);
-    myRect = new Rectangle(460, 100, 'a');
-    myRect.draw(context);
-    myRect = new Rectangle(660, 100, 'a');
-    myRect.draw(context);
-    myRect = new Rectangle(860, 100, 'a');
-    myRect.draw(context);
-    myRect = new Rectangle(1060, 100, 'a');
-    myRect.draw(context);
+class Arrow {
+    constructor(x1, y1, x2, y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.ready = 1;
+        this.xanim = 0;
+        this.yanim = 0;
+    }
+    draw() {
+        context.beginPath();
+        context.lineWidth = 2;
+        //Linea principal
+        context.moveTo(this.x1, this.y1);
+        context.lineTo(this.x2 - 2, this.y2);
+
+        //Linea superior de la flecha
+        context.moveTo(this.x2 - 4, this.y2);
+        context.lineTo(this.x2 - 9, this.y2 - 7);
+
+        //Linea inferior de la flecha
+        context.moveTo(this.x2 - 4, this.y2);
+        context.lineTo(this.x2 - 9, this.y2 + 7);
+
+        context.stroke();
+    }
+}
+
+class Ground {
+    constructor(x1, y1, x2, y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.xanim = 0;
+        this.yanim = 0;
+    }
+    draw() {
+        context.beginPath();
+        context.lineWidth = 2;
+
+        context.moveTo(this.x1, this.y1);
+        context.lineTo(this.x2 + 60, this.y2);
+
+        context.moveTo(this.x1 + 60, this.y1);
+        context.lineTo(this.x2 + 60, 170);
+
+        context.moveTo(this.x1 + 45, this.y1 + 40);
+        context.lineTo(this.x2 + 75, this.y2 + 40);
+
+        context.moveTo(this.x1 + 50, this.y1 + 44);
+        context.lineTo(this.x2 + 70, this.y2 + 44);
+
+        context.moveTo(this.x1 + 55, this.y1 + 48);
+        context.lineTo(this.x2 + 65, this.y2 + 48);
+
+        context.stroke();
+    }
+}
+
+function insertEnd(code) {
+    if (attempt != 6) {
+        activeButton(true);
+        if (attempt == 0)
+            document.getElementById("code").innerHTML = code + insertar_lvacia;
+        else
+            document.getElementById("code").innerHTML = code + insertar_lf;
+
+        let number = parseInt(document.getElementById("number").value);
+        let xaux = xposx + (attempt * 200);
+
+        let rect = new Rectangle(xaux, yposy, number);
+        //Dibujar flecha
+        if (attempt > 0) {
+            deleteGround(rect);
+            arrows(rect);
+        }
+
+        attempt++;
+
+        //Dibujar rectangulo
+        rect.draw(context);
+        rectArray.push(rect);
+
+        let updateRect = function() {
+            if (rect.ancho == 70 && rect.alto == 60) {
+                rect.show = 1; //una vez cargado el circulo muestra el numero
+                cancelAnimationFrame(updateRect);
+                activeButton(false);
+            } else requestAnimationFrame(updateRect);
+            update(rect);
+            var rectF = rectArray[attempt - 1];
+            ground(rectF);
+        }
+        updateRect();
+
+        //localStorage.setItem("qcircle" + attempt, number);
+    } else return alert("La lista esta llena y no es posible agregar más nodos.")
+}
+
+
+function update(rect) {
+    context.clearRect(rect.xpos - 3, rect.ypos - 3, 140, 65);
+    rect.draw();
+    rect.alto += 6;
+    rect.ancho += 7;
+}
+
+function invers(rect) {
+    context.clearRect(rect.xpos - 3, rect.ypos - 1, 160, 75);
+    rect.show = 0;
+    rect.ypos -= 10;
+    rect.draw(context);
+}
+
+function deleteFirst(code) {
+    document.getElementById("code").innerHTML = code + eliminar_ini;
+    let ar = arrowArray.length;
+    if (attempt != 0) {
+        rect = rectArray[0];
+        let discardRect = function() {
+            if (rect.ypos > -62) {
+                activeButton(true);
+                requestAnimationFrame(discardRect);
+            } else {
+                let r = rectArray.length;
+                for (let x = 0; x < r; x++) {
+                    if (x + 1 != r) {
+                        rectArray[x] = rectArray[x + 1];
+                    } else rectArray.pop();
+                }
+                if (ar == 0)
+                    activeButton(false);
+                else
+                    arrowDelete();
+
+                cancelAnimationFrame(discardRect);
+            }
+            invers(rect);
+        }
+        discardRect();
+        //rectArray.pop();
+        attempt--;
+    } else return alert("Error. No hay nodos existentes");
+}
+
+function deleteEnd(code) {
+    document.getElementById("code").innerHTML = code + eliminar_lpp_end;
+
+    if (attempt != 0) {
+        rect = rectArray[attempt - 1];
+        let discardRect = function() {
+            if (rect.ypos > -62) {
+                activeButton(true);
+                requestAnimationFrame(discardRect);
+            } else {
+                activeButton(false);
+                cancelAnimationFrame(discardRect);
+            }
+            invers(rect);
+        }
+        discardRect();
+        rectArray.pop();
+        attempt--;
+    } else return alert("Error. No hay nodos existentes");
+}
+
+function arrows(rect) {
+    console.log(rect.xpos, rect.ypos);
+    let xaux = rect.xpos,
+        yaux = rect.ypos + 30,
+        saux = xposx + (attempt * 200) - 95;
+    // circle.xaux = xaux;
+    let ar = new Arrow(saux, yaux, xaux, yaux);
+    ar.draw();
+    ar.ready = 0;
+    arrowArray.push(ar);
+}
+
+function ground(rect) {
+    let xaux = rect.xpos + 105,
+        yaux = rect.ypos + 30,
+        saux = xposx + (attempt * 200) - 95;
+
+    let g = new Ground(saux, yaux, xaux, yaux);
+    g.draw();
+    g.ready = 0;
+    arrowArray.push(g)
+}
+
+function deleteGround(rect) {
+    let xaux = rect.xpos + 105,
+        yaux = rect.ypos + 30,
+        saux = xposx + (attempt * 200) - 58;
+    context.clearRect(saux, yaux, 60, 90);
+}
+
+function arrowDelete() {
+    let arr = arrowArray[0];
+    arr.xanim = 1;
+    arr.yanim = 30;
+    let arrowDel = function() {
+        if (arr.ready == 1) {
+            // activeButton(false);
+            let szc = arrowArray.length;
+
+            for (let x = 0; x < szc; x++) {
+                if (x + 1 != szc) {
+                    arrowArray[x] = arrowArray[x + 1];
+                } else arrowArray.pop();
+            }
+
+            updateAll();
+            cancelAnimationFrame(arrowDel);
+        } else {
+            // activeButton(true);
+            requestAnimationFrame(arrowDel);
+        }
+        arrowDeleteAnimation(arr);
+
+    }
+    arrowDel();
+}
+
+function arrowDeleteAnimation(arr) {
+    context.clearRect(arr.x1 + 10, arr.y1 - 10, arr.xanim, arr.yanim);
+    arr.xanim += 2;
+    let distancia = arr.x2 - arr.x1 - 11;
+    if (arr.xanim >= distancia)
+        arr.ready = 1;
+}
+
+
+function updateAll() {
+    // if(arrowArray.length == 0)
+    let rect = rectArray[0];
+    let xaux = rect.xpos - 200;
+    let arrsz = arrowArray.length;
+    let recsz = rectArray.length;
+
+    let mover = function() {
+        if (rect.xpos <= xaux) {
+            activeButton(false);
+            cancelAnimationFrame(mover);
+        } else {
+            requestAnimationFrame(mover);
+        }
+
+        context.clearRect(0, 0, width, height);
+
+        for (let x = 0; x < recsz; x++) {
+            rectArray[x].xpos -= 10;
+            rectArray[x].alto = 60;
+            rectArray[x].ancho = 70;
+            rectArray[x].draw();
+        }
+        if (arrsz != 0) {
+            for (let x = 0; x < arrsz; x++) {
+                arrowArray[x].x1 -= 10;
+                arrowArray[x].x2 -= 10;
+                arrowArray[x].draw();
+
+            }
+        }
+    }
+    mover();
+
 }
 
 function activeButton(available) { //se desactivan los botones
@@ -174,36 +437,15 @@ function activeButton(available) { //se desactivan los botones
     for (i = 0; i < button.length; i++) button[i].disabled = available;
 }
 
-function insertEnd(code) {
-    if (attempt != 6) {
-        let number = parseInt(document.getElementById("number").value);
-        let rect = new Rectangle(60, 100, number);
-        rectArray.push(rect);
-        rect.draw(context);
-        /*let updateRect = function() {
-            if (rect.xpos == 60) {
-                activeButton(false);
-                cancelAnimationFrame(updateRect);
-                /*arrows(circle.ypos);*/
-        /*context.clearRect(180, 625, 50, 50)*/
-        /* } else {
-        activeButton(true);
-        requestAnimationFrame(updateRect);
-    }
-    update(rect);
-}
-console.log(rect.ypos);
-updateRect(); * /
-/*localStorage.setItem("circle" + attempt, number);*/
-        document.getElementById("code").innerHTML = code + insertar_lf;
-        attempt++;
-    } else return alert("La pila esta llena. No es posible agregar más nodos.")
-}
-
-
 function loadEvents() {
+    xposx = 60;
+    yposy = height / 2.5;
     var code = document.getElementById("code").innerHTML;
     let insert = document.getElementById("if");
+    let delete_end = document.getElementById("eu");
+    let delete_first = document.getElementById("ep");
     insert.addEventListener("click", function() { insertEnd(code); }, false);
+    delete_end.addEventListener("click", function() { deleteEnd(code); }, false);
+    delete_first.addEventListener("click", function() { deleteFirst(code); }, false);
 }
 window.addEventListener("load", loadEvents, false);
--- js/queue.js
+++ js/queue.js
@@ -15,32 +15,32 @@ var circleArray = new Array();
 var arrowArray = new Array();
 
 
-class Circle{
-    constructor(xpos,ypos,number){
-        this.xpos=xpos;
-        this.ypos=ypos;
-        this.number=number;
+class Circle {
+    constructor(xpos, ypos, number) {
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.number = number;
         // this.dy=8-((attempt)*(yCoordinate));
         this.radious = 0;
         this.show = 0;
         // this.flecha = new Arrow(0,0,0,0);
         // this.xaux = 0;
     }
-    draw(){
+    draw() {
         context.beginPath();
-        context.textAlign="center";
-        context.textBaseline="middle";
-        context.fillStyle="black";
-        context.font="20px Arial";
-        if(this.show == 1) context.fillText(this.number,this.xpos,this.ypos);
-        context.lineWidth=1;
-        context.arc(this.xpos,this.ypos,this.radious,0,Math.PI * 2,false);
+        context.textAlign = "center";
+        context.textBaseline = "middle";
+        context.fillStyle = "black";
+        context.font = "20px Arial";
+        if (this.show == 1) context.fillText(this.number, this.xpos, this.ypos);
+        context.lineWidth = 1;
+        context.arc(this.xpos, this.ypos, this.radious, 0, Math.PI * 2, false);
         context.stroke();
     }
 }
 
-class Arrow{
-    constructor(x1, y1, x2, y2){
+class Arrow {
+    constructor(x1, y1, x2, y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
@@ -49,60 +49,61 @@ class Arrow{
         this.xanim = 0;
         this.yanim = 0;
     }
-    draw(){
+    draw() {
         context.beginPath();
-        context.lineWidth=2;
+        context.lineWidth = 2;
 
-//Linea principal
+        //Linea principal
         context.moveTo(this.x1, this.y1);
-        context.lineTo(this.x2-30, this.y2);
-        
-//Linea superior de la flecha
-        context.moveTo(this.x2-32, this.y2);
-        context.lineTo(this.x2-37, this.y2-7);
+        context.lineTo(this.x2 - 30, this.y2);
 
-//Linea inferior de la flecha
-        context.moveTo(this.x2-32, this.y2);
-        context.lineTo(this.x2-37, this.y2+7);
+        //Linea superior de la flecha
+        context.moveTo(this.x2 - 32, this.y2);
+        context.lineTo(this.x2 - 37, this.y2 - 7);
+
+        //Linea inferior de la flecha
+        context.moveTo(this.x2 - 32, this.y2);
+        context.lineTo(this.x2 - 37, this.y2 + 7);
 
         context.stroke();
     }
 }
 
-function loadEvent(){
-     code = document.getElementById("code").innerHTML;
+function loadEvent() {
+    code = document.getElementById("code").innerHTML;
 
-     windoWidth = window.innerWidth;
-     windoHeight = window.innerHeight;
+    windoWidth = window.innerWidth;
+    windoHeight = window.innerHeight;
 
-     let canvas = document.getElementById("diagrama-cv");
-     context = canvas.getContext("2d");
-//Modificar tamaño del canvas
-     let wd = windoWidth, wh = windoHeight*0.32;
-     canvas.width = wd;
-     canvas.height = wh;
-     canvas.style.background="#ff8";
+    let canvas = document.getElementById("diagrama-cv");
+    context = canvas.getContext("2d");
+    //Modificar tamaño del canvas
+    let wd = windoWidth,
+        wh = windoHeight * 0.32;
+    canvas.width = wd;
+    canvas.height = wh;
+    canvas.style.background = "#ff8";
 
-//Cordenadas iniciales
-     xposx = wd/8;
-     yposy = wh/2;
+    //Cordenadas iniciales
+    xposx = wd / 8;
+    yposy = wh / 2;
 
-//Cargar los botones
+    //Cargar los botones
     let option = document.getElementsByClassName('div-opt');
     option[0].addEventListener("click", enqueve, false);
-    option[1].addEventListener("click", dequeve, false); 
+    option[1].addEventListener("click", dequeve, false);
     option[2].addEventListener("click", isEmpty, false);
     option[3].addEventListener("click", loadQueue, false);
 }
 
-function activeButton(available){
-    let button=document.getElementsByClassName("div-opt");
-    for(i=0;i<button.length;i++) button[i].disabled=available;
+function activeButton(available) {
+    let button = document.getElementsByClassName("div-opt");
+    for (i = 0; i < button.length; i++) button[i].disabled = available;
 }
 //Agregar nodo
-function enqueve(){
-    if(attempt!=7){
-        activeButton(true); 
+function enqueve() {
+    if (attempt != 7) {
+        activeButton(true);
         attempt++;
         document.getElementById("code").innerHTML = code + code_enqueve;
         let number = parseInt(document.getElementById("valor").value);
@@ -110,67 +111,63 @@ function enqueve(){
 
         let circle = new Circle(xaux, yposy, number);
         //Dibujar flecha
-        if(attempt>1) arrows(circle);
+        if (attempt > 1) arrows(circle);
         //Dibujar circulo
         circle.draw(context);
         circleArray.push(circle);
 
-        let updateCircle = function(){
-            if(circle.radious == 30){
-                circle.show = 1; //una vez cargado el circulo muestra el numero
-                cancelAnimationFrame(updateCircle);
-                activeButton(false);
+        let updateCircle = function() {
+                if (circle.radious == 30) {
+                    circle.show = 1; //una vez cargado el circulo muestra el numero
+                    cancelAnimationFrame(updateCircle);
+                    activeButton(false);
+                } else requestAnimationFrame(updateCircle);
+
+                update(circle);
             }
-            else requestAnimationFrame(updateCircle);
-
-            update(circle);
-        }
-        // console.log(circle.ypos);
+            // console.log(circle.ypos);
         updateCircle();
-        localStorage.setItem("qcircle"+attempt,number);
-    }
-    else return alert("La cola esta llena. No es posible agregar más nodos.")
+        localStorage.setItem("qcircle" + attempt, number);
+    } else return alert("La cola esta llena. No es posible agregar más nodos.")
 }
 
-function update(circle){
-        context.clearRect(circle.xpos-31,circle.ypos-39,63,72);
-        circle.draw();
-        circle.radious+=1;
+function update(circle) {
+    context.clearRect(circle.xpos - 31, circle.ypos - 39, 63, 72);
+    circle.draw();
+    circle.radious += 1;
 }
 
-function invers(circle){
+function invers(circle) {
     // if(attempt>1) xsiz = 80+circleArray[attempt]
-    context.clearRect(circle.xpos-35, circle.ypos-35, 70, 70);
+    context.clearRect(circle.xpos - 35, circle.ypos - 35, 70, 70);
     circle.show = 0;
     circle.ypos -= 5;
     circle.draw(context);
     // console.log(circle.xpos)
 }
 
-function dequeve(){
+function dequeve() {
     document.getElementById("code").innerHTML = code + code_dequeve;
     let sza = arrowArray.length;
-    if(attempt!=0){
+    if (attempt != 0) {
         activeButton(true);
 
         circle = circleArray[0];
-        let discardCircle = function(){
-            if(circle.ypos > -31){
+        let discardCircle = function() {
+            if (circle.ypos > -31) {
                 requestAnimationFrame(discardCircle);
-            }
-            else{
+            } else {
 
                 let szc = circleArray.length;
-                for(let x = 0; x<szc; x++){
-                    if(x+1 != szc){
-                        circleArray[x] = circleArray[x+1];
-                    }
-                    else circleArray.pop();
+                for (let x = 0; x < szc; x++) {
+                    if (x + 1 != szc) {
+                        circleArray[x] = circleArray[x + 1];
+                    } else circleArray.pop();
                 }
 
-                if(sza == 0)
+                if (sza == 0)
                     activeButton(false);
-                else 
+                else
                     arrowDelete();
 
 
@@ -181,36 +178,33 @@ function dequeve(){
         }
         discardCircle();
         localStorage.removeItem("qcircle1");
-        for(let x = 1; x<attempt; x++){
-            localStorage.setItem("qcircle"+x, localStorage.getItem("qcircle"+(x+1)));
+        for (let x = 1; x < attempt; x++) {
+            localStorage.setItem("qcircle" + x, localStorage.getItem("qcircle" + (x + 1)));
         }
-        localStorage.removeItem("qcircle"+attempt);
+        localStorage.removeItem("qcircle" + attempt);
         attempt--;
-    }
-    else return alert("Error. No hay nodos existentes");
+    } else return alert("Error. No hay nodos existentes");
 }
 
-function arrowDelete(){
+function arrowDelete() {
     let arr = arrowArray[0];
     arr.xanim = 1;
     arr.yanim = 30;
 
-    let arrowDel = function(){
-        if(arr.ready == 1){
+    let arrowDel = function() {
+        if (arr.ready == 1) {
             // activeButton(false);
             let szc = arrowArray.length;
 
-            for(x = 0; x<szc; x++){
-                if(x+1 != szc){
-                    arrowArray[x] = arrowArray[x+1];
-                }
-                else arrowArray.pop();
+            for (x = 0; x < szc; x++) {
+                if (x + 1 != szc) {
+                    arrowArray[x] = arrowArray[x + 1];
+                } else arrowArray.pop();
             }
 
             updateAll();
             cancelAnimationFrame(arrowDel);
-        }
-        else{
+        } else {
             // activeButton(true);
             requestAnimationFrame(arrowDel);
         }
@@ -220,71 +214,72 @@ function arrowDelete(){
 
 }
 
-function arrowDeleteAnimation(arr){
-    context.clearRect(arr.x1, arr.y1-10, arr.xanim, arr.yanim);
-    arr.xanim+=5;
+function arrowDeleteAnimation(arr) {
+    context.clearRect(arr.x1, arr.y1 - 10, arr.xanim, arr.yanim);
+    arr.xanim += 5;
     let distancia = arr.x2 - arr.x1 - 31;
-    if(arr.xanim >= distancia)
+    if (arr.xanim >= distancia)
         arr.ready = 1;
 }
 
-function updateAll(){
+function updateAll() {
     // if(arrowArray.length == 0)
     let cir = circleArray[0];
     let xaux = cir.xpos - xposx;
     let arrsz = arrowArray.length;
     let crsz = circleArray.length;
 
-    let mover = function(){
-        if(cir.xpos <= xaux){
+    let mover = function() {
+        if (cir.xpos <= xaux) {
             activeButton(false);
             cancelAnimationFrame(mover);
-        }else{
+        } else {
             requestAnimationFrame(mover);
         }
 
-        context.clearRect(0,0, windoWidth, windoHeight);
+        context.clearRect(0, 0, windoWidth, windoHeight);
 
-        for(let x = 0; x<crsz; x++){
+        for (let x = 0; x < crsz; x++) {
             circleArray[x].xpos -= 5;
             circleArray[x].draw();
         }
-        if(arrsz != 0){
-           for(let x = 0; x<arrsz; x++){
-               arrowArray[x].x1-=5;
-               arrowArray[x].x2-=5;
-               arrowArray[x].draw();
-           } 
+        if (arrsz != 0) {
+            for (let x = 0; x < arrsz; x++) {
+                arrowArray[x].x1 -= 5;
+                arrowArray[x].x2 -= 5;
+                arrowArray[x].draw();
+            }
         }
     }
     mover();
 }
 
-function isEmpty(){
+function isEmpty() {
     document.getElementById("code").innerHTML = code + code_isEmpty;
-    if(attempt == 0)
+    if (attempt == 0)
         alert("La cola esta vacia.");
     else
-        alert("La cola tiene: "+attempt+" nodos.");
+        alert("La cola tiene: " + attempt + " nodos.");
 }
 
-function loadQueue(){
-    context.clearRect(0,0, windoWidth, windoHeight);
-    attempt=0;
+function loadQueue() {
+    context.clearRect(0, 0, windoWidth, windoHeight);
+    attempt = 0;
     //nueva instancia
-    circleArray=[];
-    arrowArray=[];
-    for(var j=1;j<8;j++){
-        if(localStorage.getItem("qcircle"+j)){
-            document.getElementById("valor").value = parseInt(localStorage.getItem("qcircle"+j));
+    circleArray = [];
+    arrowArray = [];
+    for (var j = 1; j < 8; j++) {
+        if (localStorage.getItem("qcircle" + j)) {
+            document.getElementById("valor").value = parseInt(localStorage.getItem("qcircle" + j));
             enqueve();
-        }
-        else break;
+        } else break;
     }
 }
 
-function arrows(circle){
-    let xaux = circle.xpos, yaux = circle.ypos, saux = ((attempt-1)*xposx)+30;
+function arrows(circle) {
+    let xaux = circle.xpos,
+        yaux = circle.ypos,
+        saux = ((attempt - 1) * xposx) + 30;
     // circle.xaux = xaux;
     let ar = new Arrow(saux, yaux, xaux, yaux);
     ar.draw();
@@ -292,14 +287,14 @@ function arrows(circle){
     arrowArray.push(ar);
 }
 
-window.addEventListener("load",loadEvent,false);
+window.addEventListener("load", loadEvent, false);
 
 //codigo que se se ejecuta al seleccionar una opcion
 let tab = "&nbsp;&nbsp;&nbsp;&nbsp;";
 let tab2 = tab + tab;
 let tab3 = tab2 + tab;
-var code_enqueve = 
-   `
+var code_enqueve =
+    `
    <div>
         <p> void insertar(int x){</p>
         <p> ${tab}struct nodo *nuevo = newNode();</p>
@@ -320,9 +315,9 @@ var code_enqueve =
         <p> ${tab}return nuevo;</p>
         <p> }</p>
    </div>
-   `; 
+   `;
 var code_dequeve =
-   `
+    `
    <div>
         <p> int extraer(){ </p>
         <p> ${tab}if (!isEmpty()){ </p>
@@ -345,9 +340,9 @@ var code_dequeve =
         <p>  ${tab2}free(bor); </p>
         <p> ${tab}}</p>
    </div>
-   `; 
+   `;
 var code_isEmpty =
-   `
+    `
    <div> 
         <p> bool isEmpty(){ </p>
         <p> ${tab}if(raiz == null)</p>
@@ -356,4 +351,4 @@ var code_isEmpty =
         <p> ${tab2}return false; </p>
         <p> } </p>
    </div> 
-   `; 
+   `;
